a7bf368c14dac41867b09b6692faeb44d1e98055
IgorGalimski <igorg@fm-games.com>
2021-12-24 15:08:51 +0200

    Init

---
+++ MyList/MyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListApp.swift
+//  MyList
+//
+//  Created by Yasser Farahi.
+//
+
+import SwiftUI
+
+@main
+struct MyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
